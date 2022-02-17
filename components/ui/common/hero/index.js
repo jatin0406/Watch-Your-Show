1fe0508db36be591eb82b8c7f618fb5e4b8e0344
jatin0406 <jatinnchauhan11@gmail.com>
2022-02-18 02:25:32 +0530

    make it better

---
+++ components/ui/common/hero/index.js
@@ -0,0 +1,19 @@
+
+
+
+export default function Hero() {
+
+  return (
+    <section className="lg:2/6 text-left my-28">
+      <div className="text-6xl font-semibold text-gray-900 leading-none">Watch new and exiting Web series</div>
+      <div className="mt-6 text-xl font-light text-true-gray-500 antialiased">Get your popcorn, your favourite series is going to start</div>
+      <div className="mt-5 sm:mt-8 flex lg:justify-start">
+        <div className="rounded-md shadow">
+          <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-yellow-800 hover:bg-yellow-900 md:py-4 md:text-lg md:px-10">
+            Get started
+          </a>
+        </div>
+      </div>
+    </section>
+  )
+}
